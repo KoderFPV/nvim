546ea72d64e5f3137da557f1a1ca743b81c706d6
Artur Słomowski <artur.slomowski@gmail.com>
2024-11-01 22:34:59 +0100

    fix debuger for e2e and units

--- lua/neo.lua
+++ lua/neo.lua
@@ -1,24 +1,37 @@
-require("neotest").setup({
-      consumers = {
-		-- add to your list of consumers
-		playwright = require('neotest-playwright.consumers').consumers,
-	},
+require("neotest").setup {
+  consumers = {
+    -- add to your list of consumers
+    playwright = require("neotest-playwright.consumers").consumers,
+  },
   adapters = {
-          require('neotest-jest')({
-                jestCommand = "npm test --",
-                jestConfigFile = "jest.config.js",
-                env = { CI = true },
+    require "neotest-jest" {
+      jestCommand = "npm test --",
+      jestConfigFile = "jest.config.js",
+      env = { CI = true },
       jest_test_discovery = true,
-                cwd = function(path)
-                        return vim.fn.getcwd()
-                end,
-          }),
-        require('neotest-playwright').adapter({
-		options = {
-	                 persist_project_selection = true,
-	                enable_dynamic_test_discovery = true,
-		        },
-		}),
-        }
-});
+      cwd = function(path) return vim.fn.getcwd() end,
+    },
+    require("neotest-playwright").adapter {
+      options = {
+        persist_project_selection = true,
+        enable_dynamic_test_discovery = true,
+        is_test_file = function(file_path)
+          local isNil = file_path
 
+          local result = file_path:find "e2e/.*%.spec%.ts$" ~= nil
+          return result
+        end,
+        experimental = {
+          telescope = {
+            -- If true, a telescope picker will be used for `:NeotestPlaywrightProject`.
+            -- Otherwise, `vim.ui.select` is used.
+            -- In normal mode, `<Tab>` toggles the project under the cursor.
+            -- `<CR>` (enter key) applies the selection.
+            enabled = true,
+            opts = {},
+          },
+        },
+      },
+    },
+  },
+}
